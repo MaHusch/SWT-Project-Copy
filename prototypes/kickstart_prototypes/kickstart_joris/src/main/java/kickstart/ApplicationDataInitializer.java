6064bc39e2543cfd2fbdeb76f93e736e08322662
JorisK <joris.kotheimer@gmx.de>
2016-11-14 18:44:14 +0100

    #2 changes to Joris' prototype (added Salary and Order processing, compatible with forwarding)

--- prototypes/kickstart_prototypes/kickstart_joris/src/main/java/kickstart/ApplicationDataInitializer.java
+++ prototypes/kickstart_prototypes/kickstart_joris/src/main/java/kickstart/ApplicationDataInitializer.java
@@ -1,62 +1,52 @@
 package kickstart;
 
-import javax.money.CurrencyUnit;
-import javax.money.MonetaryAmount;
+import java.time.Duration;
 
 import org.javamoney.moneta.Money;
 import org.salespointframework.accountancy.Accountancy;
 import org.salespointframework.accountancy.AccountancyEntry;
 import org.salespointframework.core.DataInitializer;
-import org.salespointframework.order.OrderManager;
+import org.salespointframework.time.BusinessTime;
+import org.salespointframework.time.Interval;
+import org.salespointframework.useraccount.Role;
+import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.UserAccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
+import kickstart.model.SalaryThread;
 
 @Component
-public class ApplicationDataInitializer implements DataInitializer{
-	
+public class ApplicationDataInitializer implements DataInitializer {
+
 	private final Accountancy accountancy;
-	//private final OrderManager orderManager;
-	//private final UserAccountManager userAccountManager;
+	private final UserAccountManager userAccountManager;
+	private final BusinessTime businessTime;
 
 	@Autowired
-	public ApplicationDataInitializer(Accountancy accountancy, OrderManager orderManager, UserAccountManager userAccountManager) {
+	public ApplicationDataInitializer(Accountancy accountancy, UserAccountManager userAccountManager,
+			BusinessTime businessTime) {
 		this.accountancy = accountancy;
-		//this.orderManager = orderManager;
-		//this.userAccountManager = userAccountManager;
-		
+		this.userAccountManager = userAccountManager;
+		this.businessTime = businessTime;
 	}
 
 	@Override
 	public void initialize() {
 
-		//final Role sellerRole = Role.of("ROLE_SELLER");
-		//UserAccount us = userAccountManager.create("peter", "123", sellerRole);
 		AccountancyEntry ace1 = new AccountancyEntry(Money.of(50, "EUR"), "Einkauf");
 		AccountancyEntry ace2 = new AccountancyEntry(Money.of(-200, "EUR"), "Diebstahl");
 		AccountancyEntry ace3 = new AccountancyEntry(Money.of(536, "EUR"), "Großbestellung");
 		accountancy.add(ace1);
 		accountancy.add(ace2);
 		accountancy.add(ace3);
-		
-		
-		
-		
-		//System.out.println(accountancy.get(ace1.getId()).get().getValue().getNumber());
-		
-		
-		
-		
+
+		UserAccount ua = userAccountManager.create("Hans", "123", Role.of("ROLE_SELLER"));
+
+		(new Thread(new SalaryThread(accountancy, businessTime))).start();
+
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-
-
-
-
-
-	
---
+++ prototypes/kickstart_prototypes/kickstart_joris/src/main/java/kickstart/model/SalaryThread.java
@@ -0,0 +1,56 @@
+package kickstart.model;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import org.javamoney.moneta.Money;
+import org.salespointframework.accountancy.Accountancy;
+import org.salespointframework.accountancy.AccountancyEntry;
+import org.salespointframework.time.BusinessTime;
+
+public class SalaryThread implements Runnable {
+
+	private final Accountancy accountancy;
+	private final BusinessTime businessTime;
+
+	// @Autowired
+	public SalaryThread(Accountancy accountancy, BusinessTime businessTime) {
+		this.accountancy = accountancy;
+		this.businessTime = businessTime;
+	}
+
+	public void run() {
+		LocalDateTime currentDate = businessTime.getTime();
+		long delay = 5;
+		long next = System.currentTimeMillis();
+		while (true) {
+			LocalDateTime newDate = businessTime.getTime();
+			if (newDate.isAfter(currentDate)
+					&& (newDate.getMonth() != currentDate.getMonth() | newDate.getYear() != currentDate.getYear())) {
+				int dateDiff = (newDate.getYear() - currentDate.getYear()) * 12 + newDate.getMonthValue()
+						- currentDate.getMonthValue();
+
+				for (int i = 0; i < dateDiff; i++) {
+					accountancy.add(new AccountancyEntry(Money.of(-300, "EUR"),
+							"Gehalt für " + Month.of((currentDate.getMonthValue() + i - 1) % 12 + 1)
+									.getDisplayName(TextStyle.FULL, Locale.GERMAN)));
+				}
+				currentDate = newDate;
+			}
+			next += delay;
+			long sleep = next - System.currentTimeMillis();
+			if (sleep > 0)
+				try {
+					Thread.sleep(sleep);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		}
+
+	}
+
+}
