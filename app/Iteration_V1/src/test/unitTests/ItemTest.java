4cf40eb17d0e8d306757bdd93d533a2b2efc8fd7
Florentin Dörre <florentin@owitsch.de>
2016-11-19 15:05:46 +0100

    #14 added test folder + unitTest package and created first unittest

---
+++ app/Iteration_V1/src/test/unitTests/ItemTest.java
@@ -0,0 +1,37 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.javamoney.moneta.Money;
+import static org.salespointframework.core.Currencies.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import kickstart.model.catalog_item.Item;
+import kickstart.model.catalog_item.ItemType;
+
+public class ItemTest {
+
+	Item i1,i2;
+	
+	@Before
+	public void setUp()
+	{
+		i1 = new Item("Beer",Money.of(3.44, EURO),ItemType.DRINK);
+		i2 = new Item("FreeBeer",Money.of(3.44, EURO),ItemType.FREEDRINK); //should turn into 0.0
+	}
+	
+	@Test
+	public void testContructor() {
+		assertEquals(i2.getPrice(),Money.of(0.0, EURO));
+		//TODO: illegalArgument bei Name = "" oder price < 0 
+	}
+	
+	@Test
+	public void testGetter()
+	{
+		assertEquals(i1.getType(),ItemType.DRINK);
+		assertEquals(i2.getType(),ItemType.FREEDRINK);
+	}
+	
+}
