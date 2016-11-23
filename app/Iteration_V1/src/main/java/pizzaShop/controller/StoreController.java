89f5736f066037f5a7b9cd1cfae66f4e1127b94e
Mel <mel-frederic.fidorra@mailbox.tu-dresden.de>
2016-11-23 16:34:25 +0100

    Pizza configurator V2
    working Pizza configurator with some feature missing

--- app/Iteration_V1/src/main/java/pizzaShop/controller/StoreController.java
+++ app/Iteration_V1/src/main/java/pizzaShop/controller/StoreController.java
@@ -3,6 +3,12 @@ package pizzaShop.controller;
 
 import java.util.Optional;
 
+import javax.money.MonetaryAmount;
+
+import org.javamoney.moneta.Money;
+import org.salespointframework.catalog.ProductIdentifier;
+import org.salespointframework.order.Cart;
+import org.salespointframework.quantity.Quantity;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.UserAccountManager;
@@ -10,11 +16,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import pizzaShop.model.catalog_item.Ingredient;
+import pizzaShop.model.catalog_item.Item;
 import pizzaShop.model.catalog_item.ItemType;
+import pizzaShop.model.catalog_item.Pizza;
 import pizzaShop.model.store.ItemCatalog;
 import pizzaShop.model.store.Store;
 
@@ -23,6 +33,7 @@ import pizzaShop.model.store.Store;
 public class StoreController {
 	
 	ItemCatalog itemCatalog;
+
 	
 	@Autowired 
 	public StoreController(ItemCatalog itemCatalog) {
@@ -42,9 +53,37 @@ public class StoreController {
 	}
 	
 	@RequestMapping(value = "/addIngredientToPizza", method = RequestMethod.POST)
-	public String addIngredientToPizza(@RequestParam("id_transmit") String id, Model model){
-		System.out.println("ID: " + id);
-		return "redirect:pizza_configurator";
+	public String addIngredientToPizza(@RequestParam("id_transmit") String ids[], @ModelAttribute Cart cart){
+		
+		Pizza newPizza;
+		
+		if (ids == null || ids.length == 0)
+			return "redirect:pizza_configurator";
+		else
+			newPizza = new Pizza("custom",Money.of(7, "EUR"));
+		
+		
+		for(int i = 0; i < ids.length; i++ ){
+			
+			Iterable<Item> items = this.itemCatalog.findAll();
+
+			for(Item item : items){
+				if(item.getId().getIdentifier().equals(ids[i])){
+		
+					MonetaryAmount itemPrice = item.getPrice();
+					String itemName = item.getName();
+					
+					Ingredient newIngredient = new Ingredient(itemName,itemPrice);
+					
+					newPizza.addIngredient(newIngredient);
+					newPizza.setPrice(newPizza.getPrice().add(itemPrice));
+				}
+			}
+		}
+		
+		cart.addOrUpdateItem(newPizza, Quantity.of(1));
+		
+		return "redirect:index";
 	}
 	
 	
