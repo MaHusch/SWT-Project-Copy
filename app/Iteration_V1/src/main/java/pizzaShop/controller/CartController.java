fd9092cf758a78ebb060a7948e5cc6e613574445
Martin Huschenbett <martin-huschenbett@web.de>
2016-11-29 13:45:27 +0100

    #15 first attempt to assign deliverer to order (doesnt work)

--- app/Iteration_V1/src/main/java/pizzaShop/controller/CartController.java
+++ app/Iteration_V1/src/main/java/pizzaShop/controller/CartController.java
@@ -1,10 +1,12 @@
 package pizzaShop.controller;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.salespointframework.catalog.ProductIdentifier;
 import org.salespointframework.order.Cart;
 import org.salespointframework.order.Order;
+import org.salespointframework.order.OrderIdentifier;
 import org.salespointframework.order.OrderManager;
 import org.salespointframework.order.OrderStatus;
 import org.salespointframework.payment.Cash;
@@ -22,10 +24,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import pizzaShop.model.actor.Customer;
+import pizzaShop.model.actor.Deliverer;
+import pizzaShop.model.actor.StaffMember;
 import pizzaShop.model.store.CustomerRepository;
 import pizzaShop.model.store.ItemCatalog;
 import pizzaShop.model.store.PizzaOrder;
 import pizzaShop.model.store.PizzaOrderRepository;
+import pizzaShop.model.store.StaffMemberRepository;
 import pizzaShop.model.store.Store;
 import pizzaShop.model.tan_management.Tan;
 import pizzaShop.model.tan_management.TanManagement;
@@ -40,48 +45,67 @@ public class CartController {
 	private final TanManagement tanManagement;
 	private final CustomerRepository customerRepository;
 	private final PizzaOrderRepository pizzaOrderRepository;
+	private final StaffMemberRepository staffMemberRepository;
 	private Optional<Customer> customer = Optional.empty();
 
 	@Autowired
-	public CartController(OrderManager<Order> orderManager, ItemCatalog itemCatalog, TanManagement tanManagement, CustomerRepository customerRepository, PizzaOrderRepository pizzaOrderRepository) {
+	public CartController(OrderManager<Order> orderManager, ItemCatalog itemCatalog, TanManagement tanManagement,
+			CustomerRepository customerRepository, PizzaOrderRepository pizzaOrderRepository,
+			StaffMemberRepository staffMemberRepository) {
 		this.orderManager = orderManager;
 		this.itemCatalog = itemCatalog;
 		this.tanManagement = tanManagement;
 		this.customerRepository = customerRepository;
 		this.pizzaOrderRepository = pizzaOrderRepository;
+		this.staffMemberRepository = staffMemberRepository;
 	}
 
 	@ModelAttribute("cart")
 	public Cart initializeCart() {
 		return new Cart();
 	}
-	
+
 	@RequestMapping("/cart")
-	public String pizzaCart(Model model)
-	{
+	public String pizzaCart(Model model) {
 		return "cart";
 	}
-	
 
 	@RequestMapping("/orders")
 	public String pizzaOrder(Model model) {
-		/*for(Customer ca : customerRepository.findAll()){
-			System.out.println("ID: "+ca.getId()+" tel: "+ca.getTelephoneNumber()+" Name: "+ca.getForename());
-		}*/
-		//System.out.println("test"+customerRepository.findOne((long) 1).getTelephoneNumber());
+		/*
+		 * for(Customer ca : customerRepository.findAll()){
+		 * System.out.println("ID: "+ca.getId()+" tel: "+ca.getTelephoneNumber()
+		 * +" Name: "+ca.getForename()); }
+		 */
+		// System.out.println("test"+customerRepository.findOne((long)
+		// 1).getTelephoneNumber());
 		model.addAttribute("items", itemCatalog.findAll());
 		model.addAttribute("orders", pizzaOrderRepository.findAll());
 		model.addAttribute("customer", customer);
-		
+
+		ArrayList<StaffMember> deliverers = new ArrayList<StaffMember>();
+
+		for (StaffMember staff : Store.staffMemberList) {
+			if (staff.getRole().getName().contains("DELIVERER")) {
+				Deliverer deliverer = (Deliverer) staff;
+				if (deliverer.getAvailable()) {
+					deliverers.add(staff);
+				}
+			}
+		}
+
+		model.addAttribute("deliverers", deliverers);
+
 		return "orders";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String addItem(@RequestParam("pid") ProductIdentifier id, @RequestParam("number") int number, @ModelAttribute Cart cart) {
-		//Assert.notNull(id, "ID must not be null!");
-		//System.out.println(id + itemCatalog.findOne(id).toString());
-		
-		if(itemCatalog.findOne(id).isPresent()){
+	public String addItem(@RequestParam("pid") ProductIdentifier id, @RequestParam("number") int number,
+			@ModelAttribute Cart cart) {
+		// Assert.notNull(id, "ID must not be null!");
+		// System.out.println(id + itemCatalog.findOne(id).toString());
+
+		if (itemCatalog.findOne(id).isPresent()) {
 			cart.addOrUpdateItem(itemCatalog.findOne(id).get(), Quantity.of(number));
 		}
 		return "redirect:catalog";
@@ -90,18 +114,19 @@ public class CartController {
 
 	@RequestMapping(value = "/checkTan", method = RequestMethod.POST)
 	public String checkTan(@RequestParam("tnumber") String telephoneNumber, @RequestParam("tan") String tanValue) {
-		
+
 		Tan tan = tanManagement.getTan(telephoneNumber);
 		if (tan.getTanNumber().equals(tanValue)) {
-			for(Customer c : customerRepository.findAll()) {
-				System.out.println("test"+c.getTelephoneNumber());
+			for (Customer c : customerRepository.findAll()) {
+				System.out.println("test" + c.getTelephoneNumber());
 				if (telephoneNumber.equals(c.getTelephoneNumber())) {
 					customer = Optional.of(c);
-					System.out.println("valid: "+tanValue+" tel: "+customer.get().getTelephoneNumber());
+					System.out.println("valid: " + tanValue + " tel: " + customer.get().getTelephoneNumber());
 				}
 			}
+		} else {
+			System.out.println("fail");
 		}
-		else{System.out.println("fail");}
 
 		return "redirect:orders";
 
@@ -112,15 +137,22 @@ public class CartController {
 		if (!userAccount.isPresent()) {
 			return "redirect:login";
 		}
-		if(customer.isPresent()){
-			PizzaOrder pizzaOrder = new PizzaOrder(userAccount.get(), Cash.CASH, tanManagement.generateNewTan(customer.get().getTelephoneNumber()));//tanManagement.getTan(customer.getTelephoneNumber()));
+		if (customer.isPresent()) {
+			PizzaOrder pizzaOrder = new PizzaOrder(userAccount.get(), Cash.CASH,
+					tanManagement.generateNewTan(customer.get().getTelephoneNumber()));// tanManagement.getTan(customer.getTelephoneNumber()));
 			cart.addItemsTo(orderManager.save(pizzaOrder.getOrder()));
 			Store.getInstance().analyzeOrder(pizzaOrderRepository.save(pizzaOrder));
 			cart.clear();
-			//customer = Optional.empty(); disabled for testing purposes
+			// customer = Optional.empty(); disabled for testing purposes
 		}
 		return "redirect:orders";
 
 	}
+	@RequestMapping(value = "/assignDeliverer", method = RequestMethod.POST)
+	public String assignDeliverer(Model model, @RequestParam String lol){// @RequestParam OrderIdentifier orderId){
+		
+		System.out.println(lol);
+		 
+		return "redirect:orders";
+	}
 }
-
--- app/Iteration_V1/src/main/java/pizzaShop/model/actor/StaffMember.java
+++ app/Iteration_V1/src/main/java/pizzaShop/model/actor/StaffMember.java
@@ -9,79 +9,68 @@ import org.salespointframework.useraccount.*;
 import pizzaShop.model.store.Store;
 
 @Entity
-public abstract class StaffMember extends Person{
-	
-	
-	@Id @GeneratedValue private long employeeID;
-	
+public abstract class StaffMember extends Person {
+
+	@Id
+	@GeneratedValue
+	private long employeeID;
+
 	private String username;
 	private String password;
-	private UserAccount userAccount; 
+	private UserAccount userAccount;
 	private Role role;
-	
-	
+
 	public StaffMember(String surname, String forename, String telephoneNumber) {
-		
+
 		super(surname, forename, telephoneNumber);
-		
-		//Store.staffMemberList.add(this); now in Controller
-		
+
+		// Store.staffMemberList.add(this); now in Controller
+
 	}
-	
-	public String getUsername(){
-		
+
+	public String getUsername() {
+
 		return this.username;
 	}
-	
-	
-	public void updateUserAccount(String username, String password, Role role){
-	
-		
-		if (this.userAccount == null){
+
+	public void updateUserAccount(String username, String password, Role role) {
+
+		if (this.userAccount == null) {
 			setUsername(username);
 			setPassword(password);
 			setRole(role);
-			
-			setUserAccount(Store.employeeAccountManager.create(this.username,this.password,this.role));
+
+			setUserAccount(Store.employeeAccountManager.create(this.username, this.password, this.role));
 			Store.employeeAccountManager.save(this.userAccount);
-		}else{
+		} else {
 			// updateUserAccount
-			
+
 		}
-				
-		
-		
-		
+
 	}
-	
-	public void setUserAccount(UserAccount userAccount)	
-	{ 
+
+	public void setUserAccount(UserAccount userAccount) {
 		this.userAccount = userAccount;
 	}
-	
-	public void setUsername(String username)			
-	{ 
+
+	public void setUsername(String username) {
 		this.username = username;
 	}
-	
-	public void setPassword(String password)			
-	{ 
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	public void setRole(Role role)						
-	{ 
+
+	public void setRole(Role role) {
 		this.role = role;
 	}
-	
-	public Role getRole()						
-	{ 
+
+	public Role getRole() {
 		return this.role;
 	}
-	
-	public UserAccount getUserAccount()					
-	{ 
+
+	public UserAccount getUserAccount() {
 		return this.userAccount;
 	}
-	
+
 }
