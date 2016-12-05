472d074a305ba8b83a79ddbd67abee04ff69b9d2
Florentin <florentin@owitsch.de>
2016-12-05 17:14:16 +0100

    #24 added option to compare ascending or descending

--- app/src/main/java/pizzaShop/model/catalog_item/PriceComparator.java
+++ app/src/main/java/pizzaShop/model/catalog_item/PriceComparator.java
@@ -2,19 +2,33 @@ package pizzaShop.model.catalog_item;
 
 import java.util.Comparator;
 
+/**
+ * class to compare 2 {@link Item} based on their price
+ * @author Florentin
+ *
+ */
 public class PriceComparator implements Comparator<Item> {
+	int ascending;
 
-	public PriceComparator() {
+	/**
+	 * 
+	 * @param ascending whether the items should be compared ascending or descending
+	 */
+	public PriceComparator(boolean ascending) {
+		if (ascending)
+			this.ascending = 1;
+		else
+			this.ascending = -1;
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public int compare(Item o1, Item o2) {
 		
-		if(o1.getPrice().isGreaterThan(o2.getPrice())) return 1;
+		if(o1.getPrice().isGreaterThan(o2.getPrice())) return ascending;
 		else if(o1.getPrice().equals(o2.getPrice())) return 0;
 		// TODO Auto-generated method stub
-		return -1;
+		return -1 * ascending;
 	}
 
 }
