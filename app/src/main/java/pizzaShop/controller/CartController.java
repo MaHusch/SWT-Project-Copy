13dd821cd7d336252c92d9525114fd3c339bfe23
Florentin <florentin@owitsch.de>
2016-12-12 21:46:06 +0100

    #21 polished cart template

--- app/src/main/java/pizzaShop/controller/CartController.java
+++ app/src/main/java/pizzaShop/controller/CartController.java
@@ -216,7 +216,7 @@ public class CartController {
 		System.out.println("cutlery ist:" + cutleryStr);
 		if (customer.isPresent()) {
 			boolean onSite = false;
-			boolean cutlery = true;
+			boolean cutlery = true;´
 			System.out.println(onSiteStr + " onSite");
 			if (onSiteStr.equals("0,1")) {
 				onSite = true;
