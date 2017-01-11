8ad73e1b07dc77f7d60291f861a2476d495036a7
JorisK <joris.kotheimer@gmx.de>
2017-01-11 14:53:08 +0100

    Seperate controller for order page

--- app/src/main/java/pizzaShop/controller/CartController.java
+++ app/src/main/java/pizzaShop/controller/CartController.java
@@ -10,7 +10,6 @@ import org.salespointframework.catalog.ProductIdentifier;
 import org.salespointframework.order.Cart;
 import org.salespointframework.order.CartItem;
 import org.salespointframework.order.Order;
-import org.salespointframework.order.OrderIdentifier;
 import org.salespointframework.order.OrderManager;
 import org.salespointframework.payment.Cash;
 import org.salespointframework.quantity.Quantity;
@@ -27,9 +26,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import pizzaShop.model.AccountSystem.Customer;
-import pizzaShop.model.AccountSystem.Deliverer;
-import pizzaShop.model.AccountSystem.StaffMember;
-import pizzaShop.model.AccountingSystem.Bill;
 import pizzaShop.model.DataBaseSystem.CustomerRepository;
 import pizzaShop.model.DataBaseSystem.ItemCatalog;
 import pizzaShop.model.DataBaseSystem.PizzaOrderRepository;
@@ -40,7 +36,6 @@ import pizzaShop.model.ManagementSystem.Tan_Management.TanManagement;
 import pizzaShop.model.OrderSystem.Item;
 import pizzaShop.model.OrderSystem.ItemType;
 import pizzaShop.model.OrderSystem.PizzaOrder;
-import pizzaShop.model.OrderSystem.PizzaOrderStatus;
 
 @Controller
 @SessionAttributes("cart")
@@ -56,7 +51,6 @@ public class CartController {
 	private Optional<Customer> customer = Optional.empty();
 	private final Store store;
 	private ErrorClass cartError;
-	private ErrorClass ordersError;
 	private boolean freeDrink = false;
 
 	@Autowired
@@ -72,7 +66,6 @@ public class CartController {
 		this.store = store;
 		this.businesstime = businesstime;
 		cartError = new ErrorClass(false);
-		ordersError = new ErrorClass(false);
 	}
 
 	@ModelAttribute("cart")
@@ -83,11 +76,11 @@ public class CartController {
 	@RequestMapping("/cart")
 	public String pizzaCart(Model model, @ModelAttribute Cart cart) {
 		model.addAttribute("items", itemCatalog.findAll());
-		
+
 		ArrayList<Item> freeDrinks = new ArrayList<Item>();
-		//TODO: use itemCatalog.findByType(ItemType.FREEDRINK)
-		for(Item i : itemCatalog.findAll()){  
-			if(i.getType().equals(ItemType.FREEDRINK))
+		// TODO: use itemCatalog.findByType(ItemType.FREEDRINK)
+		for (Item i : itemCatalog.findAll()) {
+			if (i.getType().equals(ItemType.FREEDRINK))
 				freeDrinks.add(i);
 		}
 		model.addAttribute("freeDrinks", freeDrinks);
@@ -95,54 +88,9 @@ public class CartController {
 		model.addAttribute("customer", customer);
 		model.addAttribute("freeDrink", freeDrink);
 		return "cart";
-		
+
 	}
 
-	@RequestMapping("/orders")
-	public String pizzaOrder(Model model) {
-
-		ArrayList<StaffMember> deliverers = new ArrayList<StaffMember>();
-
-		for (StaffMember staff : store.getStaffMemberList()) {
-			if (staff.getRole().getName().contains("DELIVERER")) {
-				Deliverer deliverer = (Deliverer) staff;
-				if (deliverer.getAvailable()) {
-					deliverers.add(staff);
-				}
-			}
-		}
-
-		ArrayList<PizzaOrder> uncompletedOrders = new ArrayList<PizzaOrder>();
-		ArrayList<PizzaOrder> completedOrders = new ArrayList<PizzaOrder>();
-		//TODO: pizzaOrderRepository can have a method ... findbyPizzaOrderStatus
-		for (PizzaOrder po : pizzaOrderRepository.findAll()) {
-			if (po.getOrderStatus().equals(PizzaOrderStatus.COMPLETED)) {
-				completedOrders.add(po);
-			} else {
-				uncompletedOrders.add(po);
-			}
-		}
-
-		model.addAttribute("uncompletedOrders", uncompletedOrders);
-		model.addAttribute("completedOrders", completedOrders);
-		model.addAttribute("deliverers", deliverers);
-		model.addAttribute("error", ordersError);
-
-		return "orders";
-	}
-
-	@RequestMapping(value = "/confirmCollection", method = RequestMethod.POST)
-	public String cofirmLocalOrder(@RequestParam("orderID") OrderIdentifier id){
-		PizzaOrder p = pizzaOrderRepository.findOne(id);
-		if(p.getOrderStatus().equals(PizzaOrderStatus.READY)){
-			ordersError.setError(false);
-			store.completeOrder(pizzaOrderRepository.findOne(id), "mitgenommen", null); //picked up order, no deliverer needed
-		}else{
-			ordersError.setError(true);
-		}
-		return "redirect:orders";
-	}
-	
 	@RequestMapping(value = "/addCartItem", method = RequestMethod.POST)
 	public String addItem(@RequestParam("pid") ProductIdentifier id, @RequestParam("number") int number,
 			@ModelAttribute Cart cart) {
@@ -158,49 +106,98 @@ public class CartController {
 
 	@RequestMapping(value = "/removeCartItem", method = RequestMethod.POST)
 	public String removeItem(@RequestParam("ciid") String cartId, @ModelAttribute Cart cart) {
-		if(cart.getItem(cartId).get().getPrice().isZero())
+		if (cart.getItem(cartId).get().getPrice().isZero())
 			freeDrink = false;
 		cart.removeItem(cartId);
-		
-	
+
 		return "redirect:cart";
 
 	}
-	
+
 	@RequestMapping(value = "/changeQuantity", method = RequestMethod.POST)
-	public String changeQuantity(@RequestParam("ciid") String cartId, @RequestParam("amount") int amount, @RequestParam("quantity") int
-			quantity, @RequestParam("pid") ProductIdentifier id, @ModelAttribute Cart cart){
+	public String changeQuantity(	@RequestParam("ciid") String cartId, 
+									@RequestParam("amount") int amount,
+									@RequestParam("quantity") int quantity, 
+									@RequestParam("pid") ProductIdentifier id,
+									@ModelAttribute Cart cart) {
 		cartError.setError(false);
-		Optional<Item> item = itemCatalog.findOne(id); 
-		if(!item.isPresent()){
+		Optional<Item> item = itemCatalog.findOne(id);
+		if (!item.isPresent()) {
 			cartError.setError(true);
 			cartError.setMessage("Produkt existiert nicht mehr!");
 			return removeItem(cartId, cart);
 		}
-		if(quantity + amount == 0){
-			
+		if (quantity + amount == 0) {
+
 			return removeItem(cartId, cart);
-		}else{
+		} else {
 			cart.addOrUpdateItem(item.get(), Quantity.of(amount));
 		}
-		if(cart.getPrice().getNumber().intValue() < 30){
+		if (cart.getPrice().getNumber().intValue() < 30) {
 			Iterator<CartItem> ci = cart.iterator();
-			while(ci.hasNext()){
+			while (ci.hasNext()) {
 				CartItem i = ci.next();
-				if(((Item) i.getProduct()).getType().equals(ItemType.FREEDRINK)){
+				if (((Item) i.getProduct()).getType().equals(ItemType.FREEDRINK)) {
 					cart.removeItem(i.getId());
 					break;
 				}
 			}
-			freeDrink=false;
+			freeDrink = false;
 		}
 		return "redirect:cart";
 	}
-	
+
 	@RequestMapping(value = "/addFreeDrink", method = RequestMethod.POST)
-	public String addFreeDrink(@RequestParam("iid") ProductIdentifier id, @ModelAttribute Cart cart){
+	public String addFreeDrink(@RequestParam("iid") ProductIdentifier id, @ModelAttribute Cart cart) {
 		cart.addOrUpdateItem(itemCatalog.findOne(id).get(), Quantity.of(1));
-		freeDrink=true;
+		freeDrink = true;
+		return "redirect:cart";
+	}
+
+	@RequestMapping(value = "/checkout", method = RequestMethod.POST)
+	public String buy(	@ModelAttribute Cart cart, 
+						@RequestParam("onSite") String onSiteStr,
+						@RequestParam("cutlery") String cutleryStr, 
+						@LoggedIn Optional<UserAccount> userAccount) {
+		if (!userAccount.isPresent()) {
+			return "redirect:login";
+		}
+		assertTrue("Checkbox liefert anderen Wert als 0 oder 1! nämlich" + onSiteStr,
+				onSiteStr.equals("0,1") | onSiteStr.equals("0"));
+		System.out.println("cutlery ist:" + cutleryStr);
+		if (customer.isPresent()) {
+			cartError.setError(false);
+			if (cart.isEmpty()) {
+				cartError.setError(true);
+				cartError.setMessage("Warenkorb ist leer!");
+				return "redirect:cart";
+			}
+
+			boolean onSite = false;
+			boolean cutlery = true;
+			System.out.println(onSiteStr + " onSite");
+			if (onSiteStr.equals("0,1")) {
+				onSite = true;
+			}
+			if (cutleryStr.equals("0"))
+				cutlery = false;
+
+			// TODO: check if customer already has a cutlery --> throw error
+			if (cutlery) {
+				// if false --> return error
+				store.lentCutlery(customer.get(), businesstime.getTime());
+			}
+
+			PizzaOrder pizzaOrder = new PizzaOrder(userAccount.get(), Cash.CASH,
+					tanManagement.generateNewTan(customer.get().getPerson().getTelephoneNumber()), onSite, customer.get());
+			cart.addItemsTo(orderManager.save(pizzaOrder.getOrder()));
+			store.analyzeOrder(pizzaOrder);
+			cart.clear();
+
+			// Bill bill = new Bill(customer.get(), pizzaOrder,
+			// businesstime.getTime());
+			// customer = Optional.empty(); disabled for testing purposes
+		}
 		return "redirect:cart";
 	}
 
@@ -214,7 +211,8 @@ public class CartController {
 				System.out.println("test" + c.getPerson().getTelephoneNumber());
 				if (telephoneNumber.equals(c.getPerson().getTelephoneNumber())) {
 					customer = Optional.of(c);
-					System.out.println("valid: " + tanValue + " tel: " + customer.get().getPerson().getTelephoneNumber());
+					System.out
+							.println("valid: " + tanValue + " tel: " + customer.get().getPerson().getTelephoneNumber());
 				}
 			}
 		} else {
@@ -225,69 +223,10 @@ public class CartController {
 		return "redirect:cart";
 
 	}
-	
+
 	@RequestMapping(value = "/logoutCustomer", method = RequestMethod.POST)
-	public String logoutCustomer(){
+	public String logoutCustomer() {
 		customer = Optional.empty();
 		return "redirect:cart";
-	}
-
-	@RequestMapping(value = "/checkout", method = RequestMethod.POST)
-	public String buy(@ModelAttribute Cart cart, @RequestParam("onSite") String onSiteStr,
-			@RequestParam("cutlery") String cutleryStr,@LoggedIn Optional<UserAccount> userAccount) {
-		if (!userAccount.isPresent()) {
-			return "redirect:login";
-		}
-		assertTrue("Checkbox liefert anderen Wert als 0 oder 1! nämlich" + onSiteStr,
-				onSiteStr.equals("0,1") | onSiteStr.equals("0"));
-		System.out.println("cutlery ist:" + cutleryStr);
-		if (customer.isPresent()) {
-			cartError.setError(false);
-			if(cart.isEmpty()){
-				cartError.setError(true);
-				cartError.setMessage("Warenkorb ist leer!");
-				return "redirect:cart";
-			}
-			
-			boolean onSite = false;
-			boolean cutlery = true;
-			System.out.println(onSiteStr + " onSite");
-			if (onSiteStr.equals("0,1")) {
-				onSite = true;
-			}
-			if(cutleryStr.equals("0")) cutlery = false;
-			
-			//TODO: check if customer already has a cutlery --> throw error
-			if(cutlery) { 
-				// if false --> return error
-				store.lentCutlery(customer.get(), businesstime.getTime());
-			}
-
-			PizzaOrder pizzaOrder = new PizzaOrder(userAccount.get(), Cash.CASH,
-					tanManagement.generateNewTan(customer.get().getPerson().getTelephoneNumber()), onSite, customer.get());// tanManagement.getTan(customer.getTelephoneNumber()));
-			cart.addItemsTo(orderManager.save(pizzaOrder.getOrder()));
-			store.analyzeOrder(pizzaOrder);
-			cart.clear();
-			
-			//Bill bill = new Bill(customer.get(), pizzaOrder, businesstime.getTime());
-			// customer = Optional.empty(); disabled for testing purposes
-		}
-		return "redirect:cart";
-
-	}
-
-	@RequestMapping(value = "/assignDeliverer", method = RequestMethod.POST)
-	public String assignDeliverer(Model model, @RequestParam("delivererName") String name,
-			@RequestParam("orderID") OrderIdentifier orderID) {// @RequestParam
-																// OrderIdentifier
-		if (name == null || name.equals("")) {
-			ordersError.setError(true);
-			ordersError.setMessage("Keinen Lieferanten ausgewählt!");
-		} else {
-			Deliverer deliverer = (Deliverer) store.getStaffMemberByForename(name);
-
-			deliverer.addOrder(orderID);
-		}
-		return "redirect:orders";
 	}
 }
