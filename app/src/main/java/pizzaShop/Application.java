a118ae41e230c89f34459c5c3417a70e4c3c4b0d
Martin Huschenbett <martin-huschenbett@web.de>
2019-10-16 10:13:17 +0200

    added some comments...

--- app/src/main/java/pizzaShop/Application.java
+++ app/src/main/java/pizzaShop/Application.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* useless commit to create new commit*/
+
 package pizzaShop;
 
 import org.salespointframework.EnableSalespoint;
